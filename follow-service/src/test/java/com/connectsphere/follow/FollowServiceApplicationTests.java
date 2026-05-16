e50c5fe72f6e44ee5ee14bf27d5c212f2e3d5b9b
Shweta Kumari <shwetakumari3405@example.com>
2026-05-16 11:55:00 +0530

    fix: force follow-service test datasource overrides

--- follow-service/src/test/java/com/connectsphere/follow/FollowServiceApplicationTests.java
+++ follow-service/src/test/java/com/connectsphere/follow/FollowServiceApplicationTests.java
@@ -5,7 +5,17 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
 @ActiveProfiles("test")
-@SpringBootTest
+@SpringBootTest(properties = {
+		"spring.datasource.url=jdbc:h2:mem:follow-service-test;MODE=MySQL;DB_CLOSE_DELAY=-1;DB_CLOSE_ON_EXIT=FALSE",
+		"spring.datasource.driver-class-name=org.h2.Driver",
+		"spring.datasource.username=sa",
+		"spring.datasource.password=",
+		"spring.jpa.hibernate.ddl-auto=create-drop",
+		"spring.jpa.properties.hibernate.dialect=org.hibernate.dialect.H2Dialect",
+		"eureka.client.enabled=false",
+		"spring.cloud.discovery.enabled=false",
+		"connectsphere.redis.enabled=false"
+})
 class FollowServiceApplicationTests {
 
 	@Test
